9fef7a2cd2ca471fb0ca62b2e88a0d2e51a0627c
Devu-Pari <maliatulpandharpur@gmail.com>
2024-08-21 08:46:47 +0530

    dyanmic dropdown handle

---
+++ tests/dynamic-dropdown.spec.js
@@ -0,0 +1,37 @@
+
+const {test,expect}=require('@playwright/test')
+
+test ('Dyanmic dropdown demo',async function({page}){
+
+    await page.goto('https://www.google.com');
+    await page.getByTitle("search").pressSequentially('Playwright',{delay:200});
+
+
+//frame handle
+
+ await page
+         .frame({name:'callout'})
+         .locator("button",{hasText:"Stay signed out"})
+         .click();
+
+    const list1=await page.$$("//div[@role='option' and @aria-label]");
+    console.log(list1.length);
+    
+    await page.pause();
+   
+    for(let i=0;i<list1.length;i++){
+        const ele=list1[i];
+        const text=await ele.textContent();
+        console.log(text);
+
+        if(text==='playwright meaning'){
+            await ele.click();
+        }
+        
+    }
+    
+    
+})
+
+
+
