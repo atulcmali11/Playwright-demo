2e6f6b125886afe55a96fd0f9626f436eb8f66bd
Devu-Pari <maliatulpandharpur@gmail.com>
2024-08-21 08:48:33 +0530

    removed some extra lines

--- tests/dynamic-dropdown.spec.js
+++ tests/dynamic-dropdown.spec.js
@@ -2,13 +2,10 @@
 const {test,expect}=require('@playwright/test')
 
 test ('Dyanmic dropdown demo',async function({page}){
-
     await page.goto('https://www.google.com');
     await page.getByTitle("search").pressSequentially('Playwright',{delay:200});
 
-
 //frame handle
-
  await page
          .frame({name:'callout'})
          .locator("button",{hasText:"Stay signed out"})
@@ -18,12 +15,10 @@ test ('Dyanmic dropdown demo',async function({page}){
     console.log(list1.length);
     
     await page.pause();
-   
     for(let i=0;i<list1.length;i++){
         const ele=list1[i];
         const text=await ele.textContent();
         console.log(text);
-
         if(text==='playwright meaning'){
             await ele.click();
         }
