a3845b552fb727d02f8c163516da14f910f816bc
Devu-Pari <maliatulpandharpur@gmail.com>
2024-08-26 10:26:53 +0530

    dynamic drop down with spice jet site

--- tests/spicejet.spec.js
+++ tests/spicejet.spec.js
@@ -1,9 +1,16 @@
 const {test,expect}=require('@playwright/test')
 
 test('Spice jet site automation',async function({page}){
-    await page.goto("https://www.spicejet.com/");
+    await page.goto("https://www.makemytrip.com/");
     let title=await page.title();
     console.log(title);
+    await page.pause();
+    await page.locator("//section[@class='modalMain tcnFooter']/span[@data-cy='closeModal']").click();
+   // let source= await page.getByPlaceholder("From").fill('ag');
+    let source= await page.locator("//div[@class='flt_fsw_inputBox searchCity inactiveWidget activeWidget']").fill('ag');
     
+    //label[@for='fromCity']
+   
+   
     
 })
