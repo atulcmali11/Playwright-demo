eb3b6b6150b5f5418a19b7ec3f2d81d82da28bee
Devu-Pari <maliatulpandharpur@gmail.com>
2024-08-26 10:26:32 +0530

    mouse actions

---
+++ tests/mouse_action.spec.js
@@ -0,0 +1,19 @@
+const {test, expect}=require('@playwright/test')
+
+test("Mouse event demo",async function({page}){
+    await page.goto("https://testautomationpractice.blogspot.com/");
+
+    let title= await page.title();
+    console.log(title);
+    await page.getByRole('button',{name:'Copy Text'}).dblclick();
+    await page.pause();
+    let value1= await page.locator('#field1').getAttribute("value")
+   // let value2=await page.locator("#field2").getAttribute("value");
+    let value2=await page.locator("#field2").inputValue();
+    await expect(value2.includes("Hello World!")).toBeTruthy();
+
+    console.log(value1,value2);
+    
+
+     
+})
