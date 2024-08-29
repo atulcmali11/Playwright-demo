8bfd5dffdbc55a9f0fbd5daac4972a2cd0981592
Devu-Pari <maliatulpandharpur@gmail.com>
2024-08-29 07:45:36 +0530

    file upload example

---
+++ tests/file-upload.spec.js
@@ -0,0 +1,10 @@
+
+const {test,expect}=require('@playwright/test');
+const { dirname } = require('path');
+
+const file1="D://LTI.txt";
+test("File upload",async function({page}){
+    await page.goto("https://the-internet.herokuapp.com/upload");
+   await page.locator("#file-upload").setInputFiles(file1);
+   await page.pause();
+})
