9f4b83a145aefe77c7c971d900f6e92cb3e2d070
Devu-Pari <maliatulpandharpur@gmail.com>
2024-09-12 07:02:54 +0530

    slider demo

--- tests/dragdrop-slider.spec.js
+++ tests/dragdrop-slider.spec.js
@@ -1,7 +1,48 @@
 
 const {test,expect}=require('@playwright/test');
 
-test("Slider demo",async function({page}){
+test("Horizonatal Slider demo",async function({page}){
 
-    await page.goto("https://the-internet.herokuapp.com/upload");
+    await page.goto("https://the-internet.herokuapp.com/horizontal_slider");
+    const slidebarLocator=await page.locator("//input[@type='range']");
+    const valuLocator= await page.locator("//span[@id='range']");
+
+    await slidebarLocator.click();
+    const value='1.5';
+
+    while(true){
+        const text=await valuLocator.textContent();
+        console.log("Slidebar value:-",text);
+        if(text===value){
+            break;
+        }
+       // await slidebarLocator.press('ArrowLeft');
+        await slidebarLocator.press('ArrowRight');
+        await page.waitForTimeout(2000);
+
+        
+    }
+
+})
+
+test.only("Slider demo1",async function({page}){
+    await page.goto("https://www.lambdatest.com/selenium-playground/drag-drop-range-sliders-demo");
+    const slidelocation= await page.locator("//*[@id='slider3']/div/input");
+    const slideval= await page.locator("#rangeSuccess");
+
+    await slidelocation.click();
+    const val1=65;
+    while(true){
+
+        const text1=await slideval.textContent();
+        console.log(text1);
+        if(text1===val1){
+            break;
+        }
+
+        await slidelocation.press('ArrowRight');
+        await page.waitForTimeout(2000);
+        
+    }
+
 })
