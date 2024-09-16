0d7ceeb1161a49bed8b008eebc191bb8bb009446
Devu-Pari <maliatulpandharpur@gmail.com>
2024-09-16 08:28:05 +0530

    POM login case

--- tests/loginpage.spec.js
+++ tests/loginpage.spec.js
@@ -1,4 +1,4 @@
-import { LoginPage } from "../y/pageobjects/login-page-object.spec";
+import { LoginPage } from "../y/pageobjects/login-page-object";
 
 const {test}=require("@playwright/test")
 
@@ -10,6 +10,7 @@ test("Login Page test case for valid credentials",async function({page}){
     await loginpage.isDisplayedLogiPageHeader();
     await loginpage.loginToApplication(username,password);
     await loginpage.isDisplayedLogiPageHeader1();
+    await loginpage.IsLoginsuccess();
     await loginpage.isLoggedOut();
     await page.waitForTimeout(2000);
 
